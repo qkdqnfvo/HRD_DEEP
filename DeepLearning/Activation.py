3e3b3783ccb1bbfa58f1c08d83751a2f8c619ba7
qkdqnfvo <qkdqnfvo@gmail.com>
2022-06-29 17:28:04 +0900

    202206291728

--- DeepLearning/Activation.py
+++ DeepLearning/Activation.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-class Activation:
+class Activation2:
     def __init__(self):
         pass
     
@@ -21,4 +21,25 @@ class Activation:
         # return np.where(x > 0, x, 0)
 
     def linear(self, x):
+        return x
+
+
+class Activation:
+    def softmax(x):
+        if x.ndim == 1:
+            x = x - max(x)
+            return np.exp(x) / np.sum(np.exp(x))
+        else:
+            x = (x.T - np.max(x, axis=1)).T
+            x = (np.exp(x).T / np.sum(np.exp(x), axis=1)).T
+            return x
+
+    def sigmoid(x):
+        return 1 / 1 + np.exp(-x)
+
+    def relu(x):
+        return np.maximum(0, x) # 0과 x 중 큰 값을 리턴(각 요소마다)
+        # return np.where(x > 0, x, 0)
+
+    def linear(x):
         return x
